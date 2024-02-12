655dac833ae4d1049d71fbb4974c5f6d2565eaa8
Realreal84 <159738110+Realreal84@users.noreply.github.com>
2024-02-11 20:00:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function revealAnswer(answer) {
     document.getElementById('catResponse').style.display = 'block';
-    document.getElementById('catResponse').innerHTML = '<img src="<div class="https://tenor.com/view/cat-jump-happy-jumping-for-joy-jumping-gif-23625303" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
+    document.getElementById('catResponse').innerHTML = '<img src="<div class="cat-jump.gif" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
     document.getElementById('answerOfCourse').style.display = 'none';
     document.getElementById('answerDuh').style.display = 'none';
 }
