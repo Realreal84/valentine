9535a8d7d889bf89882d0b3f61126c220cebbfd0
Realreal84 <159738110+Realreal84@users.noreply.github.com>
2024-02-11 19:45:47 -0500

    Update and rename script.js to styles.css

--- script.js
+++
@@ -1,6 +0,0 @@
-function revealAnswer(answer) {
-    document.getElementById('catResponse').style.display = 'block';
-    document.getElementById('catResponse').innerText = 'YAYAYAYAYAYAYAYAY :)';
-    document.getElementById('answerOfCourse').style.display = 'none';
-    document.getElementById('answerDuh').style.display = 'none';
-}
