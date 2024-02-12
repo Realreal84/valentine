ce042ca44e36cfac7671a2c13e4717bb4ba9511e
Realreal84 <159738110+Realreal84@users.noreply.github.com>
2024-02-11 20:03:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function revealAnswer(answer) {
     document.getElementById('catResponse').style.display = 'block';
-    document.getElementById('catResponse').innerHTML = '<img src="<div class="cat-jump.gif" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
+    document.getElementById('catResponse').innerHTML = '<img src="cat-jump.gif" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
     document.getElementById('answerOfCourse').style.display = 'none';
     document.getElementById('answerDuh').style.display = 'none';
 }
