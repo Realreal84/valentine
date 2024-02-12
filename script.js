76f6a31bcded41db45a83e0c87063eb4fda08938
Realreal84 <159738110+Realreal84@users.noreply.github.com>
2024-02-11 19:34:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function revealAnswer(answer) {
+    document.getElementById('catResponse').style.display = 'block';
+    document.getElementById('catResponse').innerText = 'YAYAYAYAYAYAYAYAY :)';
+    document.getElementById('answerOfCourse').style.display = 'none';
+    document.getElementById('answerDuh').style.display = 'none';
+}
