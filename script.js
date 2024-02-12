587a3b59274a6722a4247125d222ac49b8858b33
Realreal84 <159738110+Realreal84@users.noreply.github.com>
2024-02-11 19:53:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function revealAnswer(answer) {
     document.getElementById('catResponse').style.display = 'block';
-    document.getElementById('catResponse').innerHTML = '<img src="https://tenor.com/view/cat-jump-happy-jumping-for-joy-jumping-gif-23625303" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
+    document.getElementById('catResponse').innerHTML = '<img src="<div class="tenor-gif-embed" data-postid="23625303" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/cat-jump-happy-jumping-for-joy-jumping-gif-23625303">Cat Jump Sticker</a>from <a href="https://tenor.com/search/cat-stickers">Cat Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>" alt="Jumping Cat" class="jumping-cat"><p class="yay-text">YAYAYAYAYAY</p>';
     document.getElementById('answerOfCourse').style.display = 'none';
     document.getElementById('answerDuh').style.display = 'none';
 }
